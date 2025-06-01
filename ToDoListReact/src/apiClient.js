2388391678b687c6f6ffaf2317743dcd911942cc
tzahalah <144803335+tzahalah@users.noreply.github.com>
2025-06-01 05:22:56 +0300

    Update apiClient.js

--- ToDoListReact/src/apiClient.js
+++ ToDoListReact/src/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // יצירת אינסטנס מותאם אישית
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: process.env.REACT_APP_API_URL
  });
 
 // הוספת Interceptor לתפיסת שגיאות
