2ffb9c30b5111965c2f02dafd3913adf2b2769a3
tzahalah <144803335+tzahalah@users.noreply.github.com>
2025-08-26 17:06:31 +0300

    Update apiClient.js

--- ToDoListReact/src/apiClient.js
+++ ToDoListReact/src/apiClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // יצירת אינסטנס מותאם אישית
 const apiClient = axios.create({
-  baseURL: "http://todolist-fullstack-10.onrender.com"
+  baseURL: "https://todolist-fullstack-10.onrender.com"
   
  });
 //baseURL: process.env.REACT_APP_API_URL
