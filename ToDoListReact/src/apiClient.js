5747d79c618ed9c89ba8327a6aa550af768a4093
tzahalah <144803335+tzahalah@users.noreply.github.com>
2025-09-11 17:56:44 +0300

    remove unnecessary ";" in apiClient

--- ToDoListReact/src/apiClient.js
+++ ToDoListReact/src/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // יצירת אינסטנס מותאם אישית
-const baseURL = process.env.REACT_APP_API_URL;
+const baseURL = process.env.REACT_APP_API_URL
 const apiClient = axios.create({
   baseURL,
   
